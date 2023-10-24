c0dd406a06bda123d47970aa2598b2aa83836157
RCKR <richard.raman@saunalahti.fi>
2023-10-24 07:52:17 +0200

    Updated main.py with PipeClass.
    Pipeclass downloads articles when using "run_pipeline". There
    is also a new method in this class "find_keywords" that allows
    for slicing the retrieved data based on specific keywords in
    title and description columns.

--- CodeBase/main.py
+++ CodeBase/main.py
@@ -1,29 +1,61 @@
 import pandas as pd
+import numpy as np
 import main_functions as mf
 
-def run_pipeline(fv_url, nr_pages: int = 1) -> list:
-    # extract first page and check page criteria
-    soup = mf.get_page(fv_url)
-    nr_pages = mf.nr_pages_within_page_scope(soup, nr_pages=nr_pages)
-    main_data_source = []
 
-    # loop through pages with (small) articles
-    for page_idx in range(1, nr_pages+1):
-        print(f'processing page: {page_idx}...')
-        # get url to next page
+class PropPipe:
+    def __init__(self, fv_url):
+        self.base_url = fv_url
+        self.data = None
+        self.current_soup = mf.get_page(fv_url)
+
+    def _update_soup(self, page_idx):
         if page_idx > 1:
-            next_page_url = f'{fv_url}/page/{page_idx}/'
-            soup = mf.get_page(next_page_url)
+            next_url = f'{self.base_url}/page/{page_idx}/'
+            self.current_soup = mf.get_page(next_url)
 
-        # article data from page
-        article_data = mf.run_single_page_pipe(soup)
-        main_data_source += article_data
+    def _update_key_word(self, title, word):
+        self.data['key_match'] = np.where(
+            self.data['title'].str.lower().str.contains(word), word, self.data['key_match']
+        )
 
-    return main_data_source
+    def _loop_through_keywords(self, list_of_words):
+        self.data['key_match'] = np.nan
+        for word in list_of_words:
+            word = word.lower()
+            self._update_key_word(title='title', word=word)
+            self._update_key_word(title='description', word=word)
+
+    def find_keywords(self, list_of_words):
+        if self.data is None:
+            raise AttributeError('No data to work on, please run "run_pipeline"')
+
+        self._loop_through_keywords(list_of_words)
+        result = self.data[self.data['key_match'] != 'nan'].copy()
+
+        self.data.drop(columns='key_match', inplace=True)
+        return result
+
+    def run_pipeline(self, nr_pages: int = 1):
+        # extract first page and check page criteria
+        nr_pages = mf.nr_pages_within_page_scope(self.current_soup, nr_pages=nr_pages)
+        main_data_source = []
+
+        # loop through pages with (small) articles
+        for page_idx in range(1, nr_pages + 1):
+            print(f'processing page: {page_idx}...')
+
+            # get url to next page
+            self._update_soup(page_idx)
+            article_data = mf.run_single_page_pipe(self.current_soup)
+            main_data_source += article_data
+
+        self.data = pd.DataFrame(main_data_source)
+        return main_data_source
+
 
 if __name__ == '__main__':
     fv_url_ = 'https://www.fastighetsvarlden.se/'
-    data = run_pipeline(fv_url_, nr_pages=3)
+    pipe = PropPipe(fv_url=fv_url_)
+    data = pipe.run_pipeline(nr_pages=3)
     df = pd.DataFrame(data)
-
-
--- CodeBase/main_functions.py
+++ CodeBase/main_functions.py
@@ -1,10 +1,9 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, ResultSet
 from time import sleep
-import bs4 as bs
 import requests
 
 
-def content_text(article_: bs.Tag, atr: str, desc: str = None, id_: str = None) -> str or None:
+def content_text(article_: Tag, atr: str, desc: str = None, id_: str = None) -> str or None:
 
     text_object = article_.find_all(atr)
     if len(text_object) == 0:
@@ -24,13 +23,15 @@ def content_text(article_: bs.Tag, atr: str, desc: str = None, id_: str = None) 
         print(f'No {desc} for article {id_}')
         return ''
 
-def get_id_from_main_page(article_: bs.Tag):
+
+def get_id_from_main_page(article_: Tag):
     article_response = requests.get(article_.get('href'))
     article_id = (BeautifulSoup(article_response.content, 'lxml')
                   .find('main', id='main').find('article').get('id'))
     return article_id
 
-def get_article_id(article_: bs.Tag):
+
+def get_article_id(article_: Tag):
     article_id = ''
     try:
         article_id = article_.find('article').get('id')
@@ -42,19 +43,21 @@ def get_article_id(article_: bs.Tag):
     finally:
         return article_id
 
-def check_if_article_is_premium(article_: bs.Tag) -> bool:
+
+def check_if_article_is_premium(article_: Tag) -> bool:
     if article_.find('article', class_='entry locked-paywalled__item'):
         return True
     return False
 
-def get_single_article_data(article_: bs.Tag) -> dict:
+
+def get_single_article_data(article_: Tag) -> dict:
     article_id = get_article_id(article_)
     article_content_link = article_.get('href')
     article_title = content_text(article_, atr='h2', desc='title', id_=article_id)
     article_description = content_text(article_, atr='p', desc='description', id_=article_id)
     premium_article = check_if_article_is_premium(article_)
 
-        # load information to dictionary
+    # load information to dictionary
     data_ = {
         'article_id': article_id,
         'title': article_title,
@@ -64,12 +67,14 @@ def get_single_article_data(article_: bs.Tag) -> dict:
     }
     return data_
 
-def find_articles_on_page(soup: bs.BeautifulSoup) -> bs.ResultSet:
+
+def find_page_articles(soup: BeautifulSoup) -> ResultSet:
     main_container = soup.find('main', id='main')
     articles = main_container.find_all('a', class_='entry__link')
     return articles
 
-def nr_pages_within_page_scope(soup: bs.BeautifulSoup, nr_pages: int) -> int:
+
+def nr_pages_within_page_scope(soup: BeautifulSoup, nr_pages: int) -> int:
     nav_links = soup.find('div', class_='nav-links')
     all_links = nav_links.find_all('a', class_='page-numbers')
     max_count = int(all_links[-2].text)
@@ -80,13 +85,14 @@ def nr_pages_within_page_scope(soup: bs.BeautifulSoup, nr_pages: int) -> int:
     return nr_pages
 
 
-def get_page(url: str) -> bs.BeautifulSoup:
+def get_page(url: str) -> BeautifulSoup:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     sleep(1)
     return soup
 
-def run_single_page_pipe(soup: bs.BeautifulSoup) -> list:
-    articles = find_articles_on_page(soup)
+
+def run_single_page_pipe(soup: BeautifulSoup) -> list:
+    articles = find_page_articles(soup)
     article_data = [get_single_article_data(article) for article in articles]
     return article_data
